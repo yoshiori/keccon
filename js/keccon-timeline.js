be407d6df8650b0225ae82161e64e371a45c2da3
Yoshiori SHOJI <yoshiori@gmail.com>
2010-06-12 02:54:05 +0900

    timeline 表示

---
+++ js/keccon-timeline.js
@@ -0,0 +1,38 @@
+(function(){
+    var COOKIE_NAME = 'since_id';
+    var growl = function(){
+        var _since = $.cookie(COOKIE_NAME)
+        $.ajax({
+            url : "http://search.twitter.com/search.json",
+            dataType : "jsonp",
+            data : {
+                q : "#keccon",
+                since_id : _since
+            },
+            success : function(json){
+                $.each(json['results'],function(i,obj){
+                    if( _since != null){
+                        $.pnotify({
+                            pnotify_title: obj['from_user'],
+                            pnotify_text: obj['text'],
+                            pnotify_notice_icon: 'ui-icon ui-icon-contact',
+                        });
+                    }
+                    var id = obj['id'];
+                    if( id > $.cookie(COOKIE_NAME) ){
+                        $.cookie(COOKIE_NAME, id, { path: '/', expires: 60 });
+                    }
+                });
+            },
+            error : function(){
+                $.pnotify({
+                    pnotify_title: 'error',
+                    pnotify_text: '取得出来ませんでした',
+                    pnotify_type: 'error',
+                });
+            }
+        });
+        setTimeout(growl,30000);
+    };
+    growl();
+})();
